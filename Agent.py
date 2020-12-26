d1fc592328464a25d6e452857ebb040650a3b950
natha <nathan.marotte@ulb.be>
2020-12-26 13:47:36 +0100

    Added base for agent and stimulus for PayoffMatrix.py

---
+++ Agent.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+# Action set = {C,D}
+
+class Agent:
+    def __init__(self, nb_actions=2, learning_rate=1, aspiration_level=None, habituation=None):  # TODO : find what value to start with for aspitation amd habituation
+        self.probabilities = np.empty(nb_actions, dtype=np.float64)
+        self.learning_rate = learning_rate
+        self.aspiration_level = aspiration_level
+        self.habituation = habituation
+
+    def update_probabilities(self, stimulus, action):
+        p = self.probabilities[action]
+        l = self.learning_rate
+        if stimulus >= 0:
+            future_prob = p + (1-p)*l*stimulus
+        else:
+            future_prob = p + p*l*stimulus
+        self.probabilities[action] = future_prob
+        # TODO : update other probabilities accordingly. So if p=[0.3, 0.6, 0.1] and p[0] goes from 0.3 to 0.6,
+        #  0.4 will remain to be split : 0.4 * 0.6/0.7 for p[1] and 0.4 * 0.1/0.7 for p[2] T
+        #  Or, since len(p) will always be 2, we can simply update the other p as the complementary prob of the one p
+        return future_prob
+
+    def update_aspiration(self, payoff):
+        h = self.habituation
+        A = self.aspiration_level
+        A = (1-h)*A + h*payoff
+        self.aspiration_level = A
+        return A
--- PayoffMatrix.py
+++ PayoffMatrix.py
@@ -8,6 +8,7 @@ def actions_list_to_index(actions_list):
 class PayoffMatrix:
     def __init__(self, r_value, s_value, t_value, p_value, **kwargs):
         self.matrix = np.empty(shape=(2, 2, 2), dtype=np.float)
+        self.matrix_values = {"R": r_value, "S": s_value, "T": t_value, "P": p_value}
         self.matrix[0, 0, 0] = self.matrix[0, 0, 1] = r_value
         self.matrix[1, 1, 0] = self.matrix[1, 1, 1] = p_value
 
@@ -15,14 +16,30 @@ class PayoffMatrix:
         self.matrix[0, 1, 1] = self.matrix[1, 0, 0] = t_value
 
     def get_payoff_for_actions_list(self, actions_list):
+        """
+        Method to get the typical payoff out of the matrix
+        :param actions_list: The list of action taken by all (2) players. Example : ['C', 'D']
+        :return: a numpy float 64
+        """
         return self.matrix[actions_list_to_index(actions_list)]
+
+    def get_stimulus_for_actions_list(self, actions_list, aspiration_level):
+        """
+        Method to get the stimulus out of the matrix
+        :param actions_list: The list of action taken by all (2) players. Example : ['C', 'D']
+        :param aspiration_level: the aspiration level of the agent
+        :return: a numpy float 64 (can be negative too !)
+        """
+        payoff = self.get_payoff_for_actions_list(actions_list)
+        den = max([value-aspiration_level for value in self.matrix_values.values()])
+        return (payoff-aspiration_level)/den
 
     def __str__(self):
         res = '\n'
         res += " |    C     |     D    |\n"
         res += "-|----------|----------|\n"
-        res += 'C|    ' + str(self.matrix[0,0,1]).rjust(5) + " |     " + str(self.matrix[0,1, 1]).rjust(5) + '|\n'
-        res += ' |' + str(self.matrix[0,0,0]).ljust(10) + "|" + str(self.matrix[0,1,0]).ljust(10) + '|\n'
+        res += 'C|    ' + str(self.matrix[0, 0, 1]).rjust(5) + " |     " + str(self.matrix[0, 1, 1]).rjust(5) + '|\n'
+        res += ' |' + str(self.matrix[0, 0, 0]).ljust(10) + "|" + str(self.matrix[0, 1, 0]).ljust(10) + '|\n'
         res += "-|----------|----------|\n"
         res += 'D|    ' + str(self.matrix[1, 0, 1]).rjust(5) + " |     " + str(self.matrix[1, 1, 1]).rjust(5) + '|\n'
         res += ' |' + str(self.matrix[1, 0, 0]).ljust(10) + "|" + str(self.matrix[1, 1, 0]).ljust(10) + '|\n'
